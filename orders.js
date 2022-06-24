4bbf9ea7ab7bc2fa2053dddf486c299cdfcadafc
Victor <v.henriquedasilva10@gmail.com>
2022-06-24 09:35:25 -0300

    primeiro commit

---
+++ orders.js
@@ -0,0 +1,51 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "95956",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "DJI Air 2S",
+    productNumber: "84475",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Lozenge Drone",
+    productNumber: "00182",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+];
